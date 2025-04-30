c8812f7e24807b61cc2a2f64a646c99304d8e4ca
Gogo Benson <151610817+Gogo602@users.noreply.github.com>
2025-04-30 07:41:38 +0300

    File:vite.config.js; added json data link

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: ' http://localhost:8000',
+        target: 'https://react-jobs-tau.vercel.app/jobs.json',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       }
